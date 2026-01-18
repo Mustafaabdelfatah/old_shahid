3922e5f2f59067b898284325761441821146d99f
Mustafaabdelfatah <mustafa.salama2608@gmail.com>
2026-01-18 17:50:28 +0200

    fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000' ,'https://backend.shahid3qar.com','https://shahid3qar.com','https://backend.shahid3qar.com/'],
 
     'allowed_origins_patterns' => [],
 
